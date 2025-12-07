3f718fb694272b0ceac6728bf9bbf5abeeae8a89
nyiyezin <nyiyezin11@gmail.com>
2025-12-07 20:05:37 +0630

    feat: add filtering by labels

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+
 export default function Issues() {
-  return (
-    <div>
-      <main>
-        <section>
-          <h1>Issues</h1>
-          <IssuesList />
-        </section>
-        <aside>
-          <LabelList />
-        </aside>
-      </main>
-    </div>
-  );
+    const [labels, setLabels] = useState([]);
+
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Issues</h1>
+                    <IssuesList />
+                </section>
+                <aside>
+                    <LabelList
+                        selected={labels}
+                        toggle={(label) =>
+                            setLabels(currentLabels => currentLabels.includes(label) ? currentLabels.filter(
+                                (currentLabel => currentLabel !== label)
+                            ) : currentLabels.concat(label))
+                        } />
+                </aside>
+            </main>
+        </div>
+    );
 }
