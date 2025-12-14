83d08364a92947f54881a7dfc62817ccf48717f8
nyiyezin <nyiyezin11@gmail.com>
2025-12-14 10:33:50 +0630

    feat: implement adding issue

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 
 export default function Issues() {
 	const [labels, setLabels] = useState([]);
@@ -26,6 +27,10 @@ export default function Issues() {
 					/>
 					<h3>Status</h3>
 					<StatusSelect value={status} onChange={(event) => setStatus(event.target.value)} />
+					<hr />
+					<Link className="button" to="/add">
+						Add Issue
+					</Link>
 				</aside>
 			</main>
 		</div>
